ba73f7227ab5d64c0db79ef4b9e88936f9341042
Derek <dhalden@gmail.com>
2014-04-20 15:55:07 -0400

    Initial concepts for Clock and Factory

--- Clock.java
+++ Clock.java
@@ -1,4 +1,40 @@
+/*
+ *  Authors: Yang Yu & Derek Halden
+ *  Team: Dang
+ *  Date: 4/15/14
+ *  
+ *  Class creates a "tick" every second on which the class will signal the 
+ *  factories, and let them know to create an object.
+ *
+ */
+
 public class Clock extends Thread
 {
+    public final int TICK = 1000;
+
+    //default clock with a TICK of 1 second
+    public void Clock()
+    {
+    }
+
+    //Class cares only about itself.Every second it returns itself
+    public void run()
+    {
+        while(true) // We might want to make this something else?
+        {
+            synchronized(this)
+            {
+                try
+                {
+                this.sleep(TICK);
+                }
+                catch(InterruptedException e)
+                {
+                    System.out.println("Error: Interrupted Exception " + e);
+                }
+                this.notifyAll();
+            }   
+        }
+    }
 
 }
--- Factory.java
+++ Factory.java
@@ -1,4 +1,60 @@
+/*
+ *  Authors: Yang Yu & Derek Halden
+ *  Team: Dang
+ *  Date: 4/15/14
+ *  
+ * A factory thread that waits for a clock signal, and then it produces an
+ * object and lets the store know that it has it.
+ */
 public class Factory extends Thread
 {
+    public Clock() clock;
+    public int supply;
+    public int maxSupply;
+    //We are going to want an array of the 
+
+    public void Factory(Clock() tick, int max)
+    {
+        this.clock = tick;
+        this.maxSupply = max;
+        this.supply = 0;
+    }
+    
+    public void run()
+    {
+        //I'm just making these all "while(true)"s we can change them later
+        while(true)
+        {
+            synchronized(clock)
+            {
+                this.wait();
+            }
+            this.produce();
+            this.notifyStore();
+        }
+
+    }
+
+
+    public void produce()
+    {
+        //This should probably syncrhonized on a shared variable for the
+        //factories. For now each factory's supply will be synchronized on
+        //itself
+        synchronized(this)
+        {
+            if(supply < maxSupply)
+            {
+                supply++;
+            }
+        }
+    }
+    
+    //Maybe we want to make a group object that signals the store when it's
+    //ready instead of just sending a signal every time something is produced?
+    public synchronized void notifyStore()
+    {
+        notify();
+    }
     
 }
