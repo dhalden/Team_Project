c421b50df07d0e50c96cf255cb6f000b7aabdf95
Derek <dhalden@gmail.com>
2014-04-29 11:59:40 -0400

    Customer finished

--- Customer.java
+++ Customer.java
@@ -1,7 +1,36 @@
 
 public class Customer extends Thread{
 	public CustomerSemaphore cs;
-	public Customer(CustomerSemaphore c){
+    public int demand;
+    public int range;
+	public Customer(CustomerSemaphore c, int r){
 		this.cs = c;
-	}
+        this.demand = (int)(Math.random() * r);
+	    range = r;
+    }
+
+    public void run()
+    {
+       while(true)
+       {
+       if(cs.order(demand))
+       {
+           this.demand = (int)(Math.random() * this.range);
+       }
+       else
+           {
+                synchronized(cs)
+                {
+                    try
+                    {
+                        wait();
+                    }
+                    catch(InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }     
+        } 
+    }
 }
